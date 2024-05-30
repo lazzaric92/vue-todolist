d375c5a6921aca1f28f259330afa9dad9a984b25
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-30 15:48:57 +0200

    added header and main, added v-for for ul li elements and v-bind:class to p element

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ const { createApp } = Vue
 createApp({
     data(){
         return {
-            
+            todoList: [
+                {
+                    text: 'buy potatoes',
+                    done: true
+                },
+                {
+                    text: 'bau',
+                    done: false
+                },
+                {
+                    text: 'miao',
+                    done: false
+                },
+                {
+                    text: 'boh',
+                    done: false
+                },
+            ]
         }
     },
     
